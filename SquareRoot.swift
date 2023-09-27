146545e9b0731c8d9a0a6ae77c85ac1b85b44f43
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2023-09-26 23:34:07 -0400

    Update SquareRoot.swift

--- SquareRoot.swift
+++ SquareRoot.swift
@@ -21,3 +21,26 @@ func SquareRoot(_ A: Int) -> Int {
 Time Complexity
 The program eliminates half of the possibilities on every execution, so that time complexity is O(log(n))
 ***/
+
+//Improvement
+func SquareRoot(_ A: Int) -> Int {
+        if A == 1 { return 1 }
+        var low = 1
+        var high = A/2
+
+        while(low<=high) {
+            var mid = (low+high)/2
+            
+            if(mid*mid > A) {
+                high = mid - 1
+            } else if (mid*mid < A) {
+                low = mid + 1
+            } else {
+                return mid
+            }
+        }
+        return -1
+}
+/***
+Time complexity remains same as previous solution
+***/
